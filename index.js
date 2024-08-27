c4283d2bc30af1083f45f6e8c7649f4a70c36cc9
enrichoalkalas07 <64083358+enrichoalkalas07@users.noreply.github.com>
2024-08-27 14:33:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const Express = require("express");
+const App = Express();
+
+App.use(Express.urlencoded({ extended: false }));
+App.use(Express.static("public"));
+
+const PORT = process.env.PORT || 6500;
+const HOST = "0.0.0.0";
+
+App.listen(PORT, HOST, () => console.log(`Server is running in port : ${PORT}`));
+
+App.get("/", async (req, res, next) => {
+    res.send({
+        message: "test here message"
+    })
 })
-app.listen(process.env.PORT || 3000)
